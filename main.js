8494613de618094f5ac680eec331901789dd4d98
ttse23 <terence.tse23@gmail.com>
2022-03-17 01:55:45 -0700

    Add new canvas function with user input prompt

--- main.js
+++ main.js
@@ -1,30 +1,46 @@
-let numRow = 16 //adjust for number of rows/columns in gridbox
-
 const container = document.querySelector("#gridContainer")
 const containerDim = document.querySelector("#gridContainer").clientHeight
-const gridDim = containerDim / numRow
 
-function createGrid() {
-    for (let i = 0; i < (numRow * numRow); i++){
+function createGrid(numRows) {
+    for (let i = 0; i < (numRows * numRows); i++){
         const newDiv = document.createElement("div")
-        newDiv.style.width = `${gridDim}px`
-        newDiv.style.height = `${gridDim}px`
+        newDiv.style.width = `${containerDim / numRows}px`
+        newDiv.style.height = `${containerDim / numRows}px`
         newDiv.className = "grid"
+        newDiv.addEventListener("mouseover", (e) => {
+            e.target.style.background = "grey"
+        })
         container.appendChild(newDiv)
     }
 }
-createGrid()
 
+let squares = 16 //default grid dimension is 16x16
+createGrid(squares)
 
-let grids = document.querySelectorAll(".grid")
-grids.forEach((grid) => {
-    grid.addEventListener("mouseover", (e) => {
-        e.target.style.background = "grey"
+// whites out all grids
+function clearGrid(){
+    document.querySelectorAll(".grid").forEach((grid) => {
+        grid.remove()
     })
-})
+}
+
+// prompt user for new side dim
+function getNewDim(){
+    let validInput = false
+    while (!validInput){
+        let squares = Number(prompt("Please enter a number of squares per side for the new grid (1-50): "))
+        if (squares >= 0 && squares <= 50){
+            validInput = true
+            return squares
+        }
+    }  
+}
+
+function newCanvas(){
+    clearGrid()
+    squares = getNewDim()
+    createGrid(squares)
+}
 
 const clearBut = document.querySelector("#clear")
-clearBut.addEventListener("click", clearGrid)
-function clearGrid(){
-    grids.forEach((grid) => grid.style.background = "white")
-}
+clearBut.addEventListener("click", newCanvas)
