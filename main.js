ccd117e2ea8a3eb431cc9dc2486c347e540f6c3d
ttse23 <terence.tse23@gmail.com>
2022-03-24 21:44:39 -0700

    Implement color selector and code slider bar and buttons

--- main.js
+++ main.js
@@ -1,57 +1,106 @@
 const container = document.querySelector("#gridContainer")
 const containerDim = document.querySelector("#gridContainer").clientHeight
 
-function createGrid(numRows) {
+
+const pickerBtn = document.querySelector("#pickerColor")
+pickerBtn.addEventListener("click", () => {
+    colorMode = "picker"
+    pickerBtn.classList.add("active")
+    rainbowBtn.classList.remove("active")
+    eraserBtn.classList.remove("active")
+})
+
+const rainbowBtn = document.querySelector("#rainbowColor")
+rainbowBtn.addEventListener("click", () => {
+    colorMode = "rainbow"
+    rainbowBtn.classList.add("active")
+    pickerBtn.classList.remove("active")
+    eraserBtn.classList.remove("active")
+})
+
+const eraserBtn = document.querySelector("#eraser")
+eraserBtn.addEventListener("click", () => {
+    colorMode = "eraser"
+    eraserBtn.classList.add("active")
+    pickerBtn.classList.remove("active")
+    rainbowBtn.classList.remove("active")
+})
+
+
+// default: 24x24 grid with color picker mode
+let squares = 24
+drawGrid(squares)
+
+const colorPicker = document.querySelector("#colorPicker")
+
+let colorMode = "picker"
+
+// initialize and declare mouseDown variable
+let mouseDown = false
+document.body.onmousedown = () => mouseDown = true
+document.body.onmouseup = () => mouseDown = false
+
+// draw grid function
+function drawGrid(numRows) {
     for (let i = 0; i < (numRows * numRows); i++){
         const newDiv = document.createElement("div")
         newDiv.style.width = `${containerDim / numRows}px`
         newDiv.style.height = `${containerDim / numRows}px`
         newDiv.className = "grid"
-        newDiv.addEventListener("mouseover", (e) => {
-            e.target.style.background = "grey"
-        })
+        newDiv.addEventListener("mousedown", paint)
+        newDiv.addEventListener("mouseover", paint)
         container.appendChild(newDiv)
     }
 }
 
-let squares = 16 //default grid dimension is 16x16
-createGrid(squares)
-
-// whites out all grids
+// clear grid function
 function clearGrid(){
     document.querySelectorAll(".grid").forEach((grid) => {
         grid.remove()
     })
 }
 
-// prompt user for new side dim
-function getNewDim(){
-    let validInput = false
-    while (!validInput){
-        let squares = Number(prompt("Please enter a number of squares per side for the new grid (1-50): "))
-        if (squares >= 0 && squares <= 50){
-            validInput = true
-            return squares
-        }
-    }  
+// new grid function
+function newGrid(){
+    clearGrid()
+    drawGrid(slider.value)
+    // colorMode = "picker"
+    // pickerBtn.classList.add("active")
+    // rainbowBtn.classList.remove("active")
+    // eraserBtn.classList.remove("active")
 }
 
-// slider
+// paint function
+function paint(e){
+    if ((e.type === "mousedown") || (mouseDown)){
+        if (colorMode === "picker"){
+            e.target.style.background = `${colorPicker.value}`
+        } else if (colorMode === "rainbow"){
+            e.target.style.background = randomRGB()
+        } else if (colorMode === "eraser"){
+            e.target.style.background = "white"
+        }
+    }
+}
+
+// random rgb generator
+function randomRGB(){
+    const randomR = Math.floor(Math.random() * 256)
+    const randomG = Math.floor(Math.random() * 256)
+    const randomB = Math.floor(Math.random() * 256)
+    return `rgb(${randomR}, ${randomG}, ${randomB})`
+}
+
+// clear grid button
+const clearBtn = document.querySelector("#clear")
+clearBtn.addEventListener("click", newGrid)
+
+// grid size slider
 const slider = document.querySelector("#gridSlider")
-const output = document.querySelector("#numRows")
+const output = document.querySelector("#gridSize")
 output.innerText = `${slider.value}x${slider.value}`
 
-slider.oninput = function() {
+slider.addEventListener("change", newGrid)
+slider.addEventListener("input", function() {
     output.innerText = `${this.value}x${this.value}`;
-}
-
-
-
-function newCanvas(){
-    clearGrid()
-    squares = getNewDim()
-    createGrid(squares)
-}
-
-const clearBut = document.querySelector("#clear")
-clearBut.addEventListener("click", newCanvas)
+})
